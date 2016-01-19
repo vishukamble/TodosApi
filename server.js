9d6df9103656947d29d7d3aefd57990d6a746275
VIshu Kamble <vishu.mb@gmail.com>
2016-01-19 13:33:19 -0500

    Add ?completed filter

--- server.js
+++ server.js
@@ -13,13 +13,25 @@ app.get('/',function (req, res) {
 	res.send('Todo API Root!');
 });
 
-//GET /todos
+//GET /todos/?completed=true
 app.get('/todos',function (req, res) {
-	res.json(todos);
+	var queryParams = req.query;
+	var filteredTodos = todos;
+
+	if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'true')
+	{
+		filteredTodos = _.where(filteredTodos, {completed: true});
+	}
+	else if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'false')
+	{
+		filteredTodos = _.where(filteredTodos, {completed: false});
+	}
+
+	res.json(filteredTodos);
 });
 
 //Get /todos/:id
-app.get('/todos/:id',function (req, res) {
+app.get('/todos/:id',function (req, res){
 	var todoid = parseInt(req.params.id,10);
 	var matched = _.findWhere(todos, {id: todoid});
 
