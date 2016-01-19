da15247acbc0ad64336bfc294980c55951e42646
VIshu Kamble <vishu.mb@gmail.com>
2016-01-19 00:34:05 -0500

    Add put method to api

--- server.js
+++ server.js
@@ -59,6 +59,35 @@ app.delete('/todos/:id', function(req, res)
 		res.status(404).json("Nos todo find with that id");
 });
 
+app.put('/todos/:id', function(req,res) {
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+	var body = _.pick(req.body, 'description', 'completed');
+	var validAttributes = {};
+
+	if (!matchedTodo) 
+	{
+		return res.status(404).send();
+	}
+
+	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) 
+	{
+		validAttributes.completed = body.completed;
+	} else if (body.hasOwnProperty('completed')) {
+		return res.status(400).send();
+	}
+
+	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) 
+	{
+		validAttributes.description = body.description;
+	} else if (body.hasOwnProperty('description')) {
+		return res.status(400).send();
+	}
+
+	_.extend(matchedTodo, validAttributes);
+	res.json(matchedTodo);
+});
+
 app.listen(PORT, function()
 	{
 		console.log('Express started at: '+PORT);
