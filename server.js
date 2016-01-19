c1034e5c92e7f72f1783b7ee6488d7a2518d28a1
VIshu Kamble <vishu.mb@gmail.com>
2016-01-18 20:56:49 -0500

    added post method

--- server.js
+++ server.js
@@ -1,21 +1,12 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-	id: 1,
-	description: 'Complete hackerrank challenge',
-	completed: false
-},
-{
-	id: 2,
-	description: 'Watch something',
-	completed: false
-},
-{
-	id: 3,
-	description: 'Debug code',
-	completed: true
-}];
+var todos = [];
+var todoNextId = 1; 
+
+app.use(bodyParser.json());
 
 app.get('/',function (req, res) {
 	res.send('Todo API Root!');
@@ -39,6 +30,20 @@ app.get('/todos/:id',function (req, res) {
 		return res.status(404).send();
 	else
 		res.json(matched);	
+});
+
+//POST /todos
+app.post('/todos', function(req,res)
+{
+	var body = req.body;
+	body.id = todoNextId;
+	++todoNextId
+	todos.push(body);
+
+	//console.log('description: '+body.description );
+	res.json(body);
+
+
 });
 
 app.listen(PORT, function()
