4b981f3f84cae21ae3cc8f0febdab351b20afa4d
VIshu Kamble <vishu.mb@gmail.com>
2016-01-21 13:36:40 -0500

    Forgot to initialize Sequelize in else. Fixed it

--- db.js
+++ db.js
@@ -11,9 +11,12 @@ if(env == 'production')
 }
 else
 {
+	sequelize = new Sequelize(undefined, undefined, undefined, 
+	{
 	'dialect': 'sqlite',
 	'storage': __dirname + '/data/dev-todo-api.sqlite'
-});
+	});
+}
 
 var db = {};
 
